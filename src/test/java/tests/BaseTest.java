29b941397f7b0720bb0d18e95ecf9a324c7ff997
vinceagwada <Vincent.C.Agwada@kp.org>
2019-01-28 16:24:42 -0800

    Updates for Zalenium

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,21 +1,29 @@
 package tests;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.Assert;
-import org.testng.annotations.*;
-import pages.*;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.HerokuAppMainPage;
 import util.JunitListener;
 import util.PropertyReader;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.*;
 
 public class BaseTest {
     public static PropertyReader propertyReader;
@@ -28,13 +36,13 @@ public class BaseTest {
     public BaseTest() {
         junitListener = new JunitListener();
         propertyReader = new PropertyReader();
-        LOGGER = LogManager.getLogger(BaseTest.class);
-        LOGGER.info("BaseTest completed");
+//        LOGGER = LogManager.getLogger(BaseTest.class);
+//        LOGGER.info("BaseTest completed");
     }
 
     @BeforeMethod(alwaysRun = true)
-    public void setUp() {
-        LOGGER.info("SetUp");
+    public void setUp() throws MalformedURLException {
+//        LOGGER.info("SetUp");
         setDriver();
         junitListener.setDriver(driver);
         homePage = new HerokuAppMainPage(driver);
@@ -42,18 +50,31 @@ public class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
-        LOGGER.info("TearDown");
-        driver.close();
-        driver.quit();
+    public void tearDown() throws InterruptedException {
+//        LOGGER.info("TearDown");
+        sleep(5000);
+//        driver.close();
+//        if (driver != null) {
+////            driver.quit();
+//        }
     }
 
-    public void setDriver() {
+    public void setDriver() throws MalformedURLException {
         String browser = propertyReader.readProperty("browser");
         long implicitWait = Integer.parseInt(propertyReader.readProperty("implicitWaitInSeconds"));
         switch (browser) {
+            case "remote":
+                DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, BrowserType.FIREFOX);
+                desiredCapabilities.setCapability(CapabilityType.PLATFORM, Platform.LINUX);
+                desiredCapabilities.setCapability("screenResolution", "1280x720");
+                URL seleniumUrl = new URL("http://localhost:4444/wd/hub");
+                URL remoteUrl = new URL("http://csc2cxn00001139.cloud.kp.org:4444/wd/hub");
+//                driver = new RemoteWebDriver(remoteUrl, desiredCapabilities);
+                driver = new RemoteWebDriver(seleniumUrl, desiredCapabilities);
+                break;
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", propertyReader.readProperty("winChromeDriver"));
+                System.setProperty("webdriver.chrome.driver", propertyReader.readProperty("macChromeDriver"));
                 driver = new ChromeDriver();
                 break;
             case "ie":
@@ -67,7 +88,7 @@ public class BaseTest {
                 throw new WebDriverException();
         }
         driver.manage().timeouts().implicitlyWait(implicitWait, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+        driver.manage().window().fullscreen();
     }
 
     public void loadUrl() {
@@ -78,13 +99,13 @@ public class BaseTest {
             connection = (HttpURLConnection) baseUrl.openConnection();
             connection.connect();
             if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                LOGGER.error("Unable to connect to the url connection");
-                LOGGER.error("Response Code " + connection.getResponseCode());
-                LOGGER.error("Response Message " + connection.getResponseMessage());
+//                LOGGER.error("Unable to connect to the url connection");
+//                LOGGER.error("Response Code " + connection.getResponseCode());
+//                LOGGER.error("Response Message " + connection.getResponseMessage());
             }
             driver.get(propertyReader.readProperty("url"));
         } catch (IOException e) {
-            LOGGER.error("URL connection error");
+//            LOGGER.error("URL connection error");
             e.printStackTrace();
         }
     }
