b1f2c9193ea7aa610a79ca718485009529c249dd
Vince Agwada <vinceagwada@gmail.com>
2019-01-31 23:15:09 -0800

    Now using webdriver-manager

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,28 +1,29 @@
 package tests;
 
+import com.machinepublishers.jbrowserdriver.JBrowserDriver;
+import com.machinepublishers.jbrowserdriver.Settings;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.HerokuAppMainPage;
 import util.JunitListener;
 import util.PropertyReader;
-import com.machinepublishers.jbrowserdriver.Timezone;
-import com.machinepublishers.jbrowserdriver.JBrowserDriver;
-import com.machinepublishers.jbrowserdriver.Settings;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -30,10 +31,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import static java.lang.Thread.*;
+import static io.github.bonigarcia.wdm.DriverManagerType.*;
+import static java.lang.Thread.sleep;
 
 public class BaseTest {
-    public static PropertyReader propertyReader;
+    static PropertyReader propertyReader;
     private static Logger LOGGER = null;
 
     public JunitListener junitListener;
@@ -60,12 +62,13 @@ public class BaseTest {
     public void tearDown() throws InterruptedException {
 //        LOGGER.info("TearDown");
         sleep(5000);
-//        driver.close();
+        driver.close();
 //        if (driver != null) {
 ////            driver.quit();
 //        }
     }
 
+    //TODO: Move this logic into DriverFactory
     public void setDriver() throws MalformedURLException {
         String browser = propertyReader.readProperty("browser");
         long implicitWait = Integer.parseInt(propertyReader.readProperty("implicitWaitInSeconds"));
@@ -73,7 +76,7 @@ public class BaseTest {
             case "remote":
                 DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                 desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, BrowserType.FIREFOX);
-                desiredCapabilities.setCapability(CapabilityType.PLATFORM, Platform.LINUX);
+                desiredCapabilities.setCapability(CapabilityType.PLATFORM_NAME, Platform.LINUX);
                 desiredCapabilities.setCapability("screenResolution", "1280x720");
                 URL seleniumUrl = new URL("http://localhost:4444/wd/hub");
                 URL remoteUrl = new URL("http://csc2cxn00001139.cloud.kp.org:4444/wd/hub");
@@ -81,38 +84,41 @@ public class BaseTest {
                 driver = new RemoteWebDriver(seleniumUrl, desiredCapabilities);
                 break;
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", propertyReader.readProperty("macChromeDriver"));
+                WebDriverManager.getInstance(CHROME).setup();
                 driver = new ChromeDriver();
                 break;
             case "chromeHeadless":
-                System.setProperty("webdriver.chrome.driver", propertyReader.readProperty("winChromeDriver"));
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--headless");
 //                options.addArguments("--disable-gpu");
 //                options.addArguments("--remote-debugging-port=9222");
+                WebDriverManager.getInstance(CHROME).setup();
                 driver = new ChromeDriver(options);
                 break;
             case "ie":
+                WebDriverManager.getInstance(IEXPLORER).setup();
                 driver = new InternetExplorerDriver();
                 break;
+            case "edge":
+                WebDriverManager.getInstance(EDGE).setup();
+                driver = new EdgeDriver();
+                break;
+            case "opera":
+                WebDriverManager.getInstance(OPERA).setup();
+                driver = new OperaDriver();
+                break;
             case "firefox":
-                System.setProperty("webdriver.firefox.driver", propertyReader.readProperty("winFirefoxDriver"));
+                WebDriverManager.getInstance(FIREFOX).setup();
                 driver = new FirefoxDriver();
                 break;
             case "firefoxHeadless":
-                System.setProperty("webdriver.firefox.driver", propertyReader.readProperty("winFirefoxDriver"));
-                //Set Firefox Headless mode as TRUE
                 FirefoxOptions ffoptions = new FirefoxOptions();
                 ffoptions.setHeadless(true);
+                WebDriverManager.getInstance(FIREFOX).setup();
                 driver = new FirefoxDriver(ffoptions);
                 break;
             case "jbrowserdriver":
                 Capabilities capabilities = Settings.builder()
-//                        .headless(false)
-//                        .saveAttachments(true)
-//                        .screen(new Dimension(1200, 700))
-//                        .ssl("trustanything")
-//                        .timezone(Timezone.ASIA_TOKYO)
                         .buildCapabilities();
                 driver = new JBrowserDriver(capabilities);
                 break;
